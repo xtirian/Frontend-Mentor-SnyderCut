cf97329f38aa1f7bd03a81631505ca063eb415bf
xTirian <tirianx@yahoo.com>
2024-01-05 20:40:11 -0300

    feat: add title component who's responsible for handle title of page an show the questions

---
+++ quiz-app/src/components/title/index.tsx
@@ -0,0 +1,15 @@
+import "./style.scss";
+
+const TitleContainer = () => {
+  return (
+    <div className="title">
+      <h1>
+        Welcome to the <br />
+        <span>Frontend Quiz!</span>
+      </h1>
+      <p>Pick a subject to get started.</p>
+    </div>
+  );
+};
+
+export default TitleContainer;
