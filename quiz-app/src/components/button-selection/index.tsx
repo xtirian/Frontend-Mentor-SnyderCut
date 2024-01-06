1b24dea06f84b9fb624820d9c44e2165b462d144
xTirian <tirianx@yahoo.com>
2024-01-06 14:47:11 -0300

    feat: add callback for changeing the subject

--- quiz-app/src/components/button-selection/index.tsx
+++ quiz-app/src/components/button-selection/index.tsx
@@ -2,19 +2,23 @@ import { HandleTheme } from "../../services/handleTheme";
 import "./style.scss";
 
 interface ButtonSelectionType {
-  image: string;
-  content: string;
-  callBack: Function
+  image: string
+  content: string
+  startCallBack: Function
+  subjectCallBack: Function
+
 }
 
-export const ButtonSelectionTitle = ({ image, content, callBack }: ButtonSelectionType) => {
+export const ButtonSelectionTitle = ({ image, content, startCallBack, subjectCallBack }: ButtonSelectionType) => {
 
   const {theme} = HandleTheme.useTheme()
 
 
 
   return (
-    <div className={`button_selection-container ${theme}`} onClick={() => callBack(content)}>
+    <div className={`button_selection-container ${theme}`} onClick={() => {
+      startCallBack()
+      subjectCallBack(content)}}>
       <div className={`button_selection-image-container ${content}`}>
         <img src={`/${image}`} alt="" />
       </div>
