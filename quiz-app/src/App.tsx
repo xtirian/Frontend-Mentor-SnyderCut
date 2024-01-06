5809d63dcce4424264b74447c75cf521bf59e3b7
xTirian <tirianx@yahoo.com>
2024-01-06 19:32:40 -0300

    adding context and cleaning app.tsx

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -1,36 +1,24 @@
-import { useState } from "react";
 import Header from "./components/header/index.tsx";
 import "./App.scss";
-import { QuizGridTitle } from "./views/quizz-grid-title.view.tsx";
+import { QuizGridTitle } from "./views/quizz-grid-title/quizz-grid-title.view.tsx";
 import { HandleTheme } from "./services/handleTheme.ts";
+import { HandleData } from "./services/handleData.ts";
+import QuizGridSubject from "./views/quiz-grid-subject/quizz-grid-subject.view.tsx";
 
 function App() {
   //HANDLE SUBJECT OF THE QUIZZ
 
   //START THE GAME
-  const [isStarted, setIsStarted] = useState(false);
-
-  //TO DEFINE THE SUBJECT OF THE GAME
-  const [subject, setSubject] = useState<string | undefined>(undefined);
-
-  const imgUrl = [
-    "icon-accessibility.svg",
-    "icon-css.svg",
-    "icon-html.svg",
-    "icon-js.svg",
-  ];
+  const { isStarted } = HandleData.getStartContext();
 
  
-
-  const {theme} = HandleTheme.useTheme()
-
-  
-
+  const { theme } = HandleTheme.useTheme();
 
   return (
     <section className={`app-container ${theme}`}>
-      <Header />
-      <QuizGridTitle />
+      <Header />      
+        {!isStarted?<QuizGridTitle />:<QuizGridSubject />}
+
     </section>
   );
 }
--- quiz-app/src/main.tsx
+++ quiz-app/src/main.tsx
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./sass/global.scss";
 import { ThemeProvider } from "./services/themeContext.tsx";
+import {
+  QuestionNumberProvider,
+  StartSubjectProvider,
+  SubjectProvider,
+} from "./services/subjectContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <StartSubjectProvider>
+        <SubjectProvider>
+          <QuestionNumberProvider>
+            <App />
+          </QuestionNumberProvider>
+        </SubjectProvider>
+      </StartSubjectProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
