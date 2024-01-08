62fbf1b03e15141418fb1d477914fd287fe2a986
xTirian <tirianx@yahoo.com>
2024-01-08 10:41:33 -0300

    minor changes in App.tsx, style and add the contexts to main.tsx

--- quiz-app/src/App.tsx
+++ quiz-app/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.scss";
 import { QuizGridTitle } from "./views/quizz-grid-title/quizz-grid-title.view.tsx";
 import { HandleTheme } from "./services/handleTheme.ts";
 import { HandleData } from "./services/handleData.ts";
-import QuizGridSubject from "./views/quiz-grid-subject/quizz-grid-subject.view.tsx";
+import QuizGame from "./views/quiz-game/quiz-game.view.tsx";
 
 function App() {
   //HANDLE SUBJECT OF THE QUIZZ
@@ -11,14 +11,12 @@ function App() {
   //START THE GAME
   const { isStarted } = HandleData.getStartContext();
 
- 
   const { theme } = HandleTheme.useTheme();
 
   return (
     <section className={`app-container ${theme}`}>
-      <Header />      
-        {!isStarted?<QuizGridTitle />:<QuizGridSubject />}
-
+      <Header />
+      {!isStarted ? <QuizGridTitle /> : <QuizGame />}
     </section>
   );
 }
