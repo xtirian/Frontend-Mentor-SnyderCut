55eb136c7d68559bdc9861aa11037d2a1754ce6f
xTirian <tirianx@yahoo.com>
2024-04-19 17:34:11 -0300

    feat: create the update file content service

---
+++ MarkBro/backendo/src/features/file/updateContent.service.ts
@@ -0,0 +1,31 @@
+import { FileModel } from "../../models/File";
+import FileRepository from "../../repositories/file.repository";
+import { ErrorPattern } from "../../services/ErroPattern.service";
+
+export class UpdateFileContent {
+  private readonly fileRepository = new FileRepository();
+  async validate({id, file_content, user_id}: FileModel): Promise<FileModel>{
+    if(!id || !file_content || !user_id){
+      throw ErrorPattern.missingRequired('Missing params');
+    }
+
+    const file = await this.fileRepository.getById(id);
+    if(!file){
+      throw ErrorPattern.notFound('File not found');
+    }
+    if(file.user_id !== user_id){
+      throw ErrorPattern.unauthorized('Unauthorized');
+    }
+    const model = new FileModel({
+      id,
+      file_content
+    });
+    await this.fileRepository.updateContent(model);
+    return model;
+  }
+
+  async execute(file: FileModel): Promise<FileModel>{
+    const updatedFile = await this.validate(file);
+    return updatedFile;
+  }
+}
