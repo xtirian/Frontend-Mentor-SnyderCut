0e9e64aac0d6d2097ed3b08b63397a1e850ee6b4
xTirian <tirianx@yahoo.com>
2024-04-20 00:14:28 -0300

    feat: create routes for files handle

---
+++ MarkBro/backendo/src/routes/auth/files/updateContent.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { handleResponse } from "../../../services/HandleResponse.service";
+import { UpdateFileContent } from "../../../features/file/updateContent.service";
+
+const router = Router();
+
+router.put("/file/update/content", async (req, res) => {
+  try {
+    const { fileId, fileContent } = req.body;
+    //
+    const userId = req.user.id;
+    const service = await new UpdateFileContent().execute({
+      id: fileId,
+      user_id: userId,
+      file_content: fileContent,
+    });
+    const response = handleResponse(service);
+    res.status(response.status).json(response.data);
+  } catch (err) {
+    const response = handleResponse(err);
+    res.status(response.status).json({
+      error: response.error,
+      details: response.details,
+      message: response.message,
+    });
+  }
+});
+
+export default router;
