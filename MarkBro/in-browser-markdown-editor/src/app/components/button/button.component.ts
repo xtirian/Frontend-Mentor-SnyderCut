41dac6a873741efa27a10d6370faf1501a8b7732
xTirian <tirianx@yahoo.com>
2024-04-29 18:16:43 -0300

    feat: add button component

---
+++ MarkBro/in-browser-markdown-editor/src/app/components/button/button.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ButtonModel } from '../../../lib/models/button.model';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() callback:ButtonModel['callback'] = ():void => {}
+  @Input() disabled:ButtonModel['disabled'] = false;
+  @Input() loading:ButtonModel['loading'] = false;
+  @Input() type:ButtonModel['type'] = 'button';
+  @Input() variant:ButtonModel['variant'] = 'button';
+  @Input() text:ButtonModel['text'] = 'Button';
+
+  ngOnInit():void{
+
+
+  }
+}
+
+
