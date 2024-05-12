9c985a001615ac405195fd31ca26fc5e655bb99c
xTirian <tirianx@yahoo.com>
2024-05-12 15:21:45 -0300

    feat: Refactor theme service to support dark mode preference and saved theme

--- MarkBro/in-browser-markdown-editor/src/app/services/theme.service.ts
+++ MarkBro/in-browser-markdown-editor/src/app/services/theme.service.ts
@@ -7,29 +7,50 @@ export class ThemeService {
   private theme: string = 'light';
 
   constructor() {
-    const verifyTheme = window.matchMedia('(prefers-color-scheme: dark)');
-    if (verifyTheme.matches) {
-      this.setTheme('dark');
-    } else {
-      this.setTheme('light');
-    }
-    verifyTheme.addEventListener('change', (e) => {
-      if (e.matches) {
+    if (typeof window !== 'undefined') {
+      const verifyTheme = window.matchMedia('(prefers-color-scheme: dark)');
+      if (verifyTheme.matches) {
         this.setTheme('dark');
       } else {
         this.setTheme('light');
       }
-    })
+      verifyTheme.addEventListener('change', (e) => {
+        console.log('Theme preference changed:', e.matches ? 'dark' : 'light');
+        if (e.matches) {
+          this.setTheme('dark');
+        } else {
+          this.setTheme('light');
+        }
+      });
+
+      const savedTheme = this.getTheme();
+      if (savedTheme) {
+        this.setTheme(savedTheme);
+      } else if (verifyTheme.matches) {
+        this.setTheme('dark');
+      } else {
+        this.setTheme('light');
+      }
+    }
   }
+
   // Function to get the theme
-  getTheme() {
-    return localStorage.getItem('theme');
+  getTheme(): 'dark' | 'light' | null{
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem('theme') as 'dark' | 'light';
+    }
+    return null;
   }
+
+
   // Function to the the theme
   setTheme(theme: string) {
     this.theme = theme;
     localStorage.setItem('theme', theme);
-    document.body.classList.remove('dark', 'light');
-    document.body.classList.add(theme);
+    if (typeof document !== 'undefined' && typeof document.body !== 'undefined') {
+      document.body.classList.remove('dark', 'light');
+      document.body.classList.add(theme);
+    }
   }
 }
+
